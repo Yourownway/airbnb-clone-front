022d56d6e12614b8525fcc5ebb10e9579f22789c
Lucas Beneston <benestonlucas@gmail.com>
2020-07-07 11:40:18 +0200

    Nouveau merge

--- app/src/components/Signin.jsx
+++ app/src/components/Signin.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Signin() {
   return (
     <div>
-      <h1>Je suis la page Signin !!</h1>
+      <h1>Je suis la page Signin !</h1>
     </div>
   );
 }
