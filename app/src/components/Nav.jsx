562eb0ec741510b0a24f301e52ab4ac1e8d226e7
Lucas Beneston <benestonlucas@gmail.com>
2020-07-09 12:56:18 +0200

    Merge master

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -3,27 +3,25 @@ import { Link } from 'react-router-dom';
 
 export default function Nav() {
   return (
-    <div>
-      <nav>
-        <div>
-          <Link to="/">
-            <img src="airbnb.png" alt="" />
-            <h3>Explorer</h3>
-          </Link>
-        </div>
-        <div>
-          <Link to="/enregistres">
-            <img src="heart.png" alt="" />
-            <h3>Enregistrés</h3>
-          </Link>
-        </div>
-        <div>
-          <Link to="/connexion">
-            <img src="user.png" alt="" />
-            <h3>Connexion</h3>
-          </Link>
-        </div>
-      </nav>
-    </div>
+    <nav>
+      <div>
+        <Link to="/">
+          <img src="airbnb.png" alt="" />
+          <h3>Explorer</h3>
+        </Link>
+      </div>
+      <div>
+        <Link to="/enregistres">
+          <img src="heart.png" alt="" />
+          <h3>Enregistrés</h3>
+        </Link>
+      </div>
+      <div>
+        <Link to="/connexion">
+          <img src="user.png" alt="" />
+          <h3>Connexion</h3>
+        </Link>
+      </div>
+    </nav>
   );
 }
