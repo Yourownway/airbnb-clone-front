cdc5bcb04015e03b49a10e701f4e1f4f36263096
Lucas Beneston <benestonlucas@gmail.com>
2020-07-13 16:40:59 +0200

    Ajout modal

--- app/src/components/Connexion.jsx
+++ app/src/components/Connexion.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Modal from '@material-ui/core/Modal';
 
-export default function Signup() {
+export default function Connexion() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="container">
-      <h1>Je suis la page Connexion !!</h1>
+    <div className="maintenance">
+      <button type="button" onClick={handleOpen}>
+        Open Modal
+      </button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        className="Modal"
+      >
+        <div className="Modal_container">
+          <div className="Modal_container_header">
+            <h1>Inscription</h1>
+          </div>
+          <form className="Modal_container_form">
+            <input type="text" name="" id="" placeholder="Nom" />
+            <input type="text" name="" id="" placeholder="Prénom" />
+            <input type="email" name="" id="" placeholder="Email" />
+            <input type="password" name="" id="" placeholder="Mot de passe" />
+            <button type="submit">Continuer</button>
+          </form>
+        </div>
+      </Modal>
     </div>
   );
 }
